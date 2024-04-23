0b1dd3aeba2b8783eb663e827626b9fb0fdfdb35
elifdonmez <donmez.elif.c@gmail.com>
2024-04-23 15:41:01 +0300

    Post page is added: User navigates to post page to see the details

--- mysite/communityinfo/views.py
+++ mysite/communityinfo/views.py
@@ -164,6 +164,10 @@ def community(request, community_name):
         return redirect('home_page')
 
 
+def post_view(request, post_id):
+    post = get_object_or_404(Posts, id=post_id)
+    return render(request, 'post.html', {'post': post})
+
 def join_community(request, community_name):
     if request.method == 'POST':
         username = request.session.get('username')
